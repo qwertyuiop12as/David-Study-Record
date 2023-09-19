f8dc0c217c7b954c65cd755cde3b5056e333007c
JaeWook Cho <78254430+qwertyuiop12as@users.noreply.github.com>
2023-09-19 10:56:56 +0900

    Update yutnori.cpp
    Road and result

--- yutnori.cpp
+++ yutnori.cpp
@@ -10,15 +10,47 @@ struct player {
 
 player p1, p2;
 
+bool check_win(int p_num, int pos_f, int road_num)
+{
+    
+}
+
 void move(int p_num, int pos_i, int num)
 {
-    int road_num = 0;
+    int road_num = 0, pos_f;
     
     if (pos_i == 11 || pos_i == 201 || pos_i == 202 || pos_i == 203 || pos_i == 204 || pos_i == 205)
         road_num = 2;
     
     else if (pos_i == 6 || pos_i == 101 || pos_i == 102 || pos_i == 103 || pos_i == 104)
         road_num = 1;
+        
+    if (road_num == 0)
+    {
+        pos_f = pos_i + num;
+    }
+    
+    else if (road_num == 1)
+    {
+        if (pos_i + num == 103)
+            pos_f = 203;
+            
+        else if (pos_i + num > 105)
+            pos_f = (pos_i + num - 105 + 15);
+            
+        else
+            pos_f = pos_i + num;
+    }
+    
+    else if (road_num == 2)
+    {
+        if (pos_i + num > 105)
+            pos_f = (pos_i + num - 105 + 15);
+            
+        else
+            pos_f = pos_i + num;
+    }
+    
 }
 
 int throw_yut(int p_num)
@@ -26,6 +58,9 @@ int throw_yut(int p_num)
     int tmp, i;
     srand (time(NULL));
     
+    // Throw 4 individual yuts
+    
+    // Use random function to decide "Front (F)" or "Back (B)"
     for (i = 0; i < 4; i++)
     {
         tmp = rand() % 2;
@@ -36,6 +71,7 @@ int throw_yut(int p_num)
             yut[i] = false;
     }
     
+    // Print the result of each yut
     int b_cnt = 0;
     for (i = 0; i < 4; i++)
     {
@@ -51,6 +87,7 @@ int throw_yut(int p_num)
     
     printf("\n");
     
+    // Based on number of front and back, decide the outcome as Do, Gae, Gul, Yut, or Mo.
     switch(b_cnt)
     {
         case 0:
